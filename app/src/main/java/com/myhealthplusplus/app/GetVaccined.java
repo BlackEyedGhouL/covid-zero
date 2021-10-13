8090d2eb930ce522289af558548a8aa5fc7f9e29
BlackEyedGhouL <34senith@gmail.com>
2021-10-13 06:36:50 +0530

    feat: validate vaccine registration

--- app/src/main/java/com/myhealthplusplus/app/GetVaccined.java
+++ app/src/main/java/com/myhealthplusplus/app/GetVaccined.java
@@ -9,9 +9,14 @@ import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 import java.util.Objects;
 
 public class GetVaccined extends AppCompatActivity {
+
+    TextInputLayout firstName, lastName, nic, postalCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +27,114 @@ public class GetVaccined extends AppCompatActivity {
         setTitle(Html.fromHtml("<font color=\"white\">" + "Vaccinations" + "</font>"));
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.dark_black)));
 
+        firstName = findViewById(R.id.get_vaccined_reg_firstName_txtLayout);
+        lastName = findViewById(R.id.get_vaccined_reg_LastName_txtLayout);
+        nic = findViewById(R.id.get_vaccined_reg_NIC_txtLayout);
+        postalCode = findViewById(R.id.get_vaccined_reg_PostalCode_txtLayout);
+
         Button next = findViewById(R.id.get_vaccined_reg_Next);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (!validateFirstName() | !validateLastName() | !validatePostalCode() | !validateNIC()) {
+                    return;
+                }
                 Intent intent = new Intent(GetVaccined.this, GetVaccined_p2.class);
                 startActivity(intent);
+            }
+
+            private boolean validateNIC() {
+                String val = nic.getEditText().getText().toString();
+                String checkForLetters = "[0-9vV]+";
+
+                if (val.isEmpty()) {
+                    nic.setError("Field can not be empty");
+                    return false;
+                }
+                else if (!val.matches(checkForLetters)) {
+                    nic.setError("Invalid NIC!");
+                    return false;
+                }
+                else if (val.length() < 10 || val.length() == 11) {
+                    nic.setError("NIC should contain 10 or 12 characters!");
+                    return false;
+                }
+                else {
+                    nic.setError(null);
+                    nic.setErrorEnabled(false);
+                    return true;
+                }
+            }
+
+            private boolean validatePostalCode() {
+                String val = postalCode.getEditText().getText().toString();
+                String checkForLetters = "[0-9]+";
+
+                if (val.isEmpty()) {
+                    postalCode.setError("Field can not be empty");
+                    return false;
+                }
+                else if (!val.matches(checkForLetters)) {
+                    postalCode.setError("Invalid postal code!");
+                    return false;
+                }
+                else if (val.length() < 5) {
+                    postalCode.setError("Postal code should contain 5 characters!");
+                    return false;
+                }
+                else {
+                    postalCode.setError(null);
+                    postalCode.setErrorEnabled(false);
+                    return true;
+                }
+            }
+
+            private boolean validateLastName() {
+                String val = lastName.getEditText().getText().toString();
+                String checkForLetters = "[a-zA-Z]+";
+                String noWhiteSpace = "\\A\\w{1,20}\\z";
+
+                if (val.isEmpty()) {
+                    lastName.setError("Field can not be empty");
+                    return false;
+                }
+                else if (!val.matches(noWhiteSpace)) {
+                    lastName.setError("No white spaces are allowed!");
+                    return false;
+                }
+                else if (!val.matches(checkForLetters)) {
+                    lastName.setError("Invalid last name!");
+                    return false;
+                }
+                else {
+                    lastName.setError(null);
+                    lastName.setErrorEnabled(false);
+                    return true;
+                }
+            }
+
+            private boolean validateFirstName() {
+                String val = firstName.getEditText().getText().toString();
+                String checkForLetters = "[a-zA-Z]+";
+                String noWhiteSpace = "\\A\\w{1,20}\\z";
+
+                if (val.isEmpty()) {
+                    firstName.setError("Field can not be empty");
+                    return false;
+                }
+                else if (!val.matches(noWhiteSpace)) {
+                    firstName.setError("No white spaces are allowed!");
+                    return false;
+                }
+                else if (!val.matches(checkForLetters)) {
+                    firstName.setError("Invalid first name!");
+                    return false;
+                }
+                else {
+                    firstName.setError(null);
+                    firstName.setErrorEnabled(false);
+                    return true;
+                }
             }
         });
     }
---
+++ app/src/main/java/com/myhealthplusplus/app/GetVaccined_Verify.java
@@ -0,0 +1,14 @@
+package com.myhealthplusplus.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetVaccined_Verify extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_vaccined_verify);
+    }
+}
